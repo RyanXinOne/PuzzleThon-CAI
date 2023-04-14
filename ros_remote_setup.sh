6a890ecb18bed3f011e8a5dc981e238b1219ad33
RyanXin <xyz@ryanxin.cn>
2023-04-14 14:10:44 +0100

    Ros remote env setup

--- ros_remote_setup.sh
+++ ros_remote_setup.sh
@@ -1,6 +1,8 @@
-export ROS_MASTER_URI=http://10.42.0.1:11311
-export ROS_IP=10.42.0.$1
+#!/bin/bash
 
-if [ -z $1 ]; then
-	echo No IP value
+x=$(ifconfig | grep -oE '10\.42\.0\.[0-9]+' | sed 's/10\.42\.0\.//' | head -n1)
+
+if [ -n "$x" ]; then
+	export ROS_MASTER_URI=http://10.42.0.1:11311
+	export ROS_IP=10.42.0.$x
 fi
