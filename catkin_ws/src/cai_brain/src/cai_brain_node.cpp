9adfab86489248d8b5cd7e6dbca72abbbf9130b6
RyanXin <xyz@ryanxin.cn>
2023-04-23 21:51:34 +0100

    Update controller

--- catkin_ws/src/cai_brain/src/cai_brain_node.cpp
+++ catkin_ws/src/cai_brain/src/cai_brain_node.cpp
@@ -4,23 +4,29 @@
 #include <sensor_msgs/Image.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
-#include <string>
+#include <vector>
 
 using namespace std;
 
+double V_INI = 0.05;
+double W_INI = 0;
+double K_P = 2;
+
+double v, w;
 double w_l, w_r;
-double v = 0.05, w = 0;
 
 void wl_callback(const std_msgs::Float32::ConstPtr &msg);
 void wr_callback(const std_msgs::Float32::ConstPtr &msg);
 void camera_callback(const sensor_msgs::ImageConstPtr &msg);
-int find_line(cv::Mat image);
+double locate_middle_lane(cv::Mat &image);
 
 int main(int argc, char *argv[])
 {
     ROS_INFO("[CaiBrain] Starting node...");
     ros::init(argc, argv, "cai_brain_node");
 
+    v = V_INI;
+    w = W_INI;
     w_l = 0;
     w_r = 0;
 
@@ -30,7 +36,7 @@ int main(int argc, char *argv[])
     ros::Subscriber sub = nh.subscribe("/camera/image_raw", 10, camera_callback);
     ros::Publisher pub_cmd_vel = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 100);
 
-    ros::Rate loop_rate(100);
+    ros::Rate loop_rate(10);
     while (ros::ok())
     {
         ros::spinOnce();
@@ -60,47 +66,36 @@ void camera_callback(const sensor_msgs::ImageConstPtr &msg)
 {
     cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
     cv::Mat image = cv_ptr->image;
-
-    // to grey
     cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
 
     // central of white region
-    int center = find_line(image);
-
-    ROS_INFO("center: %d", center);
-
-    if (center < image.cols / 2)
+    double mid_lane = locate_middle_lane(image);
+    ROS_INFO("middle lane: %lf", mid_lane);
+    if (mid_lane > -1)
     {
-        w = 0.15;
+        double error = mid_lane - 0.5;
+        w = -K_P * error;
     }
-    else
-    {
-        w = -0.15;
-    }
-    cv::imshow("result", image);
 
+    cv::imshow("camera", image);
     cv::waitKey(1);
 }
 
-// define a function, input a image and output an int
-int find_line(cv::Mat image){
-    // check the white region at the bottom line
-    int left = 0;
-    int right = 0;
-    bool is_left = false;
-    for (int i = image.cols / 4; i < image.cols * 3 / 4; i++)
+// find middle lane position [0, 1] in input image
+double locate_middle_lane(cv::Mat &image)
+{
+    int margin = image.cols / 5;
+    int threshold = 150;
+
+    vector<int> pixel_candidates;
+    for (int i = margin; i < image.cols - margin; i++)
     {
-        if (image.at<uchar>(image.rows - 1, i) >= 127 && !is_left)
+        // last row as the scan line
+        if (image.at<uchar>(image.rows - 1, i) > threshold)
         {
-            left = i;
-            is_left = true;
-            // break;
-        }
-        if (image.at<uchar>(image.rows - 1, i) < 127 && is_left)
-        {
-            right = i-1;
-            break;
+            pixel_candidates.push_back(i);
         }
     }
-    return (left+right)/2;
+    // return middle candidates
+    return !pixel_candidates.empty() ? pixel_candidates[pixel_candidates.size() / 2] / (double)(image.cols - 1) : -1;
 }
