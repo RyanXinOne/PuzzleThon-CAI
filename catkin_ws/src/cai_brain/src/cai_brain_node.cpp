0b30d7c5d6177611a24e472dfcd7362532182a9c
MorganREN <mohan.ren@student.manchester.ac.uk>
2023-04-23 10:18:47 -0700

    Auto-drive on the road

--- catkin_ws/src/cai_brain/src/cai_brain_node.cpp
+++ catkin_ws/src/cai_brain/src/cai_brain_node.cpp
@@ -4,12 +4,17 @@
 #include <sensor_msgs/Image.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
+#include <string>
+
+using namespace std;
 
 double w_l, w_r;
+double v = 0.05, w = 0;
 
 void wl_callback(const std_msgs::Float32::ConstPtr &msg);
 void wr_callback(const std_msgs::Float32::ConstPtr &msg);
 void camera_callback(const sensor_msgs::ImageConstPtr &msg);
+int find_line(cv::Mat image);
 
 int main(int argc, char *argv[])
 {
@@ -26,8 +31,6 @@ int main(int argc, char *argv[])
     ros::Publisher pub_cmd_vel = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 100);
 
     ros::Rate loop_rate(100);
-    double v = 0.1;
-    double w = 0;
     while (ros::ok())
     {
         ros::spinOnce();
@@ -56,8 +59,48 @@ void wr_callback(const std_msgs::Float32::ConstPtr &msg)
 void camera_callback(const sensor_msgs::ImageConstPtr &msg)
 {
     cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-
     cv::Mat image = cv_ptr->image;
-    cv::imshow("Camera Image", image);
+
+    // to grey
+    cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
+
+    // central of white region
+    int center = find_line(image);
+
+    ROS_INFO("center: %d", center);
+
+    if (center < image.cols / 2)
+    {
+        w = 0.15;
+    }
+    else
+    {
+        w = -0.15;
+    }
+    cv::imshow("result", image);
+
     cv::waitKey(1);
+}
+
+// define a function, input a image and output an int
+int find_line(cv::Mat image){
+    // check the white region at the bottom line
+    int left = 0;
+    int right = 0;
+    bool is_left = false;
+    for (int i = image.cols / 4; i < image.cols * 3 / 4; i++)
+    {
+        if (image.at<uchar>(image.rows - 1, i) >= 127 && !is_left)
+        {
+            left = i;
+            is_left = true;
+            // break;
+        }
+        if (image.at<uchar>(image.rows - 1, i) < 127 && is_left)
+        {
+            right = i-1;
+            break;
+        }
+    }
+    return (left+right)/2;
 }
